a83ca0302607b1638376f1b457f643cc3ccd3588
Breyanna Apolito <93007297+breyannamariee@users.noreply.github.com>
2024-05-18 00:04:55 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,6 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-      
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
  
   
   useEffect(() => {
@@ -23,14 +18,18 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-                onClick={() => deleteTodo(todo.id)}
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id}>{todo.content}
+          onClick={() => deleteTodo(todo.id)}</li>
         ))}
       </ul>
       <div>
