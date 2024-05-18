8337b2c852123c01ca69b11b0a531dfe4686786d
Breyanna Apolito <93007297+breyannamariee@users.noreply.github.com>
2024-05-17 23:52:18 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ const client = generateClient<Schema>();
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+      
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+ 
+  
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -23,6 +29,7 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
+                onClick={() => deleteTodo(todo.id)}
           <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
